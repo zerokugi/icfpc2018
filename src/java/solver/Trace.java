75c0650e30a8627e2410a55a3bba72979545d6c8
zerokugi <zerokugi@gmail.com>
2018-07-21 05:09:00 +0900

    fix Trace as 4 vals

--- src/java/solver/Trace.java
+++ src/java/solver/Trace.java
@@ -5,17 +5,23 @@ public class Trace {
     public final Type type;
     public final Integer val0;
     public final Integer val1;
+    public final Integer val2;
+    public final Integer val3;
 
-    public Trace(final Type type, final int val0, final int val1) {
+    public Trace(final Type type, final Integer val0, final Integer val1, final Integer val2, final Integer val3) {
         this.type = type;
         this.val0 = val0;
         this.val1 = val1;
+        this.val2 = val2;
+        this.val3 = val3;
     }
 
     public Trace(final Type type, final int val0) {
-        this.type = type;
-        this.val0 = val0;
-        val1 = null;
+        this(type, val0, null, null, null);
+    }
+
+    public Trace(final Type type, final int val0, final int val1) {
+        this(type, val0, val1, null, null);
     }
 
     enum Type {
